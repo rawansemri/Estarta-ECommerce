9217b3a64330d77d11e5bbbd8d0432a4ba6c2482
Rawan Abusemri <RawanA@estartasolutions.com>
2023-03-14 14:54:42 +0300

    add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import {lazy, Suspense} from 'react'
 import { LoginPage } from "./pages/Login";
 import { Home } from "./pages/Home";
@@ -13,6 +13,12 @@ import { Spinner } from './components/Spinner'
 // const Products = lazy(() => import("./pages/Products"));
 // const NotFound = lazy(() => import("./pages/NotFound"));
 // const LoginPage = lazy(() => import("./pages/Login"));
+function ProtectedRoute({props}){
+  const {isAuth} = useSelector(state=>state.authReducer);
+
+  if (isAuth) {return element} else {<Navigate to='/login'/>}
+
+}
 function App() {
 const {loading} = useSelector(state=>state.authReducer)
 if(loading) return (<Spinner />)
@@ -25,8 +31,8 @@ if(loading) return (<Spinner />)
       <Routes>
         <Route index element={<Home/>} />
         <Route path="/login" element={<LoginPage/>} />
-        <Route path="/products" element={<Products/>} />
-        <Route path="/carts" element={<Cart/>} />
+        <Route path="/products" element={<ProtectedRoute element= {<Products/>} />} />
+        <Route path="/carts" element={<ProtectedRoute element= {<Cart/>} />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
       </BrowserRouter>
@@ -36,5 +42,6 @@ if(loading) return (<Spinner />)
     
   )
 }
+
 
 export default App
